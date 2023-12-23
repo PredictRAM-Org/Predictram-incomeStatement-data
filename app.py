c7a97516679257951fd650c310b8af66b6f3f2d4
PredictRAM DataHub <95133921+PredictRAM-Org@users.noreply.github.com>
2023-12-23 23:55:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
 import streamlit as st
 import os
 import pandas as pd
+import json
 
 def load_data(stock_folder, selected_stock):
     file_path = os.path.join(stock_folder, f"{selected_stock}.json")
     if os.path.exists(file_path):
-        data = pd.read_json(file_path)
-        return data
+        try:
+            with open(file_path, 'r') as f:
+                data = json.load(f)
+            if isinstance(data, list) and all(isinstance(entry, dict) for entry in data):
+                return pd.json_normalize(data)
+            else:
+                st.warning("Invalid JSON format. Please check the structure of your JSON file.")
+                return None
+        except Exception as e:
+            st.error(f"Error reading JSON file: {str(e)}")
+            return None
     else:
+        st.warning(f"No data found for {selected_stock}")
         return None
 
 def main():
@@ -29,7 +40,7 @@ def main():
         st.write(f"Displaying data for {selected_stock}")
         st.write(stock_data)
     else:
-        st.warning(f"No data found for {selected_stock}")
+        st.warning(f"No valid data found for {selected_stock}")
 
 if __name__ == "__main__":
     main()
